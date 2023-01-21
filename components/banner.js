1303af03762442d95a4161112fcff6719803cc73
muhammed-gumus <mr.silver.mg@gmail.com>
2023-01-22 01:50:49 +0300

    started to responsive design

--- components/banner.js
+++ components/banner.js
@@ -4,13 +4,13 @@ import Link from 'next/link'
 export default function Media () {
     return(
         <div className={utilStyles.banner}>
-            <Image className={utilStyles.BannerProfile} src="/images/profile.jpg" width={"450px"} height={"450px"} />
+            <img className={utilStyles.BannerProfile} src="/images/profile.jpg" />
             <div className={utilStyles.BannerRight}>
                 <div className={utilStyles.BannerTexts}>
                     <p className={utilStyles.BannerTitle}>Frontend Developer</p>
                     <p className={utilStyles.BannerName}>Muhammed Gümüş</p>
-                    <p className={utilStyles.BannerAbout}>Hi. I'm Muhammed. I am a 3rd year Computer Engineering student. I have been <br/> 
-                    continuing my programming adventure, which started 2 years ago, as a Frontend<br/>  
+                    <p className={utilStyles.BannerAbout}>Hi. I'm Muhammed. I am a 3rd year Computer Engineering student. I have been
+                    continuing my programming adventure, which started 2 years ago, as a Frontend 
                     Developer for 1 year. I am open to learning and development.</p>
                 </div>
                 <div>
