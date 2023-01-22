c2c08e6778e32ab8d04d7a46dd86468e83370c2e
muhammed-gumus <mr.silver.mg@gmail.com>
2023-01-23 01:53:22 +0300

    completed: home page responsive design

--- components/navbar.js
+++ components/navbar.js
@@ -5,21 +5,24 @@ export default function Media() {
     <div className={utilStyles.navbar}>
       <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
         <h1>Muhammed Gümüş</h1>
-        
       </div>
       <div className={utilStyles.navbarList}>
-        <a href="/" className={utilStyles.navbarListItem}>
-          Home
-        </a>
-        <a href="FrontendSkills" className={utilStyles.navbarListItem}>
-          Tech Stacks
-        </a>
-        <a href="Portfolio" className={utilStyles.navbarListItem}>
-          Portfolio
-        </a>
-        <a href="Contact" className={utilStyles.navbarListItem}>
-          Contact
-        </a>
+        <div>
+          <a href="/" className={utilStyles.navbarListItem}>
+            Home
+          </a>
+          <a href="FrontendSkills" className={utilStyles.navbarListItem}>
+            Tech Stacks
+          </a>
+        </div>
+        <div>
+          <a href="Portfolio" className={utilStyles.navbarListItem}>
+            Portfolio
+          </a>
+          <a href="Contact" className={utilStyles.navbarListItem}>
+            Contact
+          </a>
+        </div>
       </div>
     </div>
   );
